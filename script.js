2b1f145c31ad6a77f5c7afc2e15f012b218180c7
Greg <greg19850@gmail.com>
2022-08-29 15:19:16 +0100

    Created event listener for window, lettersKeyHandle, handling keydown event for letter keys

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const words = ['programmer', 'notification', 'workout', 'television', 'bodybuild
 const hiddenWord = words[Math.floor(Math.random() * words.length)];
 
 const correctLetters = [];
-const incorrectLetters = [];
+const wrongLetters = [];
 
 function displayWord() {
   wordElement.innerHTML = `
@@ -33,5 +33,34 @@ function displayWord() {
     popup.style.display = 'flex';
   }
 }
+
+function letterKeysHandle(e) {
+  if (e.keyCode >= 65 && e.keyCode <= 95) {
+    const letter = e.key;
+
+    if (hiddenWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter)
+
+        displayWord()
+      } else {
+        showNotification()
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter)
+
+        updateWrongLettersElement()
+      } else {
+        showNotification()
+      }
+    }
+
+
+  }
+}
+
+
+window.addEventListener('keydown', letterKeysHandle)
 
 displayWord()
